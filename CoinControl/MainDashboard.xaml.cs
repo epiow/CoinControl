b87324cbb18a13fd87da36e187c64051fc63585f
saydough <garpshyperx@yahoo.com>
2024-04-23 01:37:16 +0800

    added current balance log in main dashboard

--- CoinControl/MainDashboard.xaml.cs
+++ CoinControl/MainDashboard.xaml.cs
@@ -37,6 +37,12 @@ namespace CoinControl
 
         public void LoadInformation()
         {
+            var user = _context.User.FirstOrDefault(u => u.User_ID == loggedInUserId);
+            if (user != null)
+            {
+                balanceText.Text = user.Balance.ToString("0.00");
+            }
+
             var allIncomes = _context.Income.Where(income => income.User_ID == loggedInUserId).ToList();
             decimal totalIncome = allIncomes.Sum(income => income.Amount);
             incomeText.Text = totalIncome.ToString("0.00");
